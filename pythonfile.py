12ef8393371a958e2414151d329419e1e524e5cd
Benjamin Rose <bros@umich.edu>
2021-04-26 15:24:44 -0400

    undoing

--- pythonfile.py
+++ pythonfile.py
@@ -74,20 +74,26 @@ class funWithTheEarth:
 
 ## Coordinates must be inputted as a tuple
 
-# input an iso2 code, returns an iso3 code
-    def getiso3fromiso2(self,iso2,file):
-        with open(file) as file2:
-            csv_reader = csv.reader(file2, delimiter=',')
-            coordlist = []
-            next(csv_reader)
-            shortest_distance = None
-            shortest_distance_coordinates = None
-            dalist = []
-            for row in csv_reader:
-                if row[1] == iso2:
-                    return row[2]
-                    
-                    
+#   yo i have no idea what this function is for, i trust you tho!
+    # def getcountryfromcoords(self,coords,file):
+    #     with open(file) as file2:
+    #         csv_reader = csv.reader(file2, delimiter=',')
+    #         coordlist = []
+    #         next(csv_reader)
+    #         shortest_distance = None
+    #         shortest_distance_coordinates = None
+    #         dalist = []
+    #         for row in csv_reader:
+    #             coordlist.append((float(row[4].strip().strip('"')),float(row[5].strip().strip('"'))))
+    #             dalist.append(row)
+    #         for coordinate in coordlist:
+    #             distance = math.sqrt(((coordinate[0]-coords[0])**2)+((coordinate[1]-coords[1])**2))
+    #             if shortest_distance is None or distance < shortest_distance:
+    #                 shortest_distance = distance
+    #                 shortest_distance_coordinates = coordinate
+    #         for country in dalist:
+    #             if float(country[4].strip().strip('"')) == shortest_distance_coordinates[0] and float(country[5].strip().strip('"')) == shortest_distance_coordinates[1]:
+    #                 return country[2]
 
     def gettemp(self,country):
         url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/{}".format(country)   
